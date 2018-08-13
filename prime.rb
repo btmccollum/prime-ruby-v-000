cbad934a6356faf93084ee927c72453215f74542
Brad McCollum <btmcco8@gmail.com>
2018-08-13 22:54:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,12 +3,22 @@ require'benchmark'
 puts Benchmark.measure{
 
 def prime?(n)
-  if n <= 1
+  if n < 2
     return false
-  else
-    (2..Math.sqrt(n)).none? { |n| (n % n).zero?}
-    Math.sqrt(n).floor.downto(2).each {|i| return false if n % i == 0}
-    true
-  end
+  else (2..upto(n/2)).each { |x| x % n == 0 }
+    return true
 end
+end
+
+
+
+# def prime?(n)
+#   if n <= 1
+#     return false
+#   else
+#     (2..Math.sqrt(n)).none? { |n| (n % n).zero?}
+#     Math.sqrt(n).floor.downto(2).each {|i| return false if n % i == 0}
+#     true
+#   end
+# end
 }
